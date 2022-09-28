149d5e75e6fca5403effbe2d457f9c0e7609501d
Lawrence <lawrence.0725@hotmail.com>
2022-09-28 14:36:41 +0800

    Add 優惠券兌換

---
+++ js/ticket.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    popConfirm(); }
+
+// Popup close
+function popClose(e) {
+    if (e.classList.contains('pop'))
+        window.onclick  = function(event) {
+            if (event.target == e) {
+                e.children[0].style.animationName = "popdown";
+                setTimeout(function(){
+                    e.style.display = "none";
+                    e.children[0].style.animationName = "popup";
+                }, 500); }}
+    else if (!(e.classList.contains('popBody'))) {
+        e.closest('.pop').children[0].style.animationName = "popdown";
+        setTimeout(function(){
+            e.closest('.pop').style.display = "none";
+            e.closest('.pop').children[0].style.animationName = "popup";
+        }, 500); }
+    document.body.style.overflow = "scroll"; }
+
+// 分類popup
+function popConfirm() {
+    document.getElementById("popConfirm").style.display = "flex";
+    document.body.style.overflow = "hidden"; }
