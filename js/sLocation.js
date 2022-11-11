e56f7c27ad901e50c652ca5af1e0446cd6fc38ef
Lawrence <lawrence.0725@hotmail.com>
2022-11-11 20:25:55 +0800

    Update 分店資訊頁 animation

--- js/sLocation.js
+++ js/sLocation.js
@@ -1,6 +1,8 @@
-function dropdown(e) {
-    if (e.classList.contains('min'))
-        e.classList.remove('min');
-    else
-        e.classList.add('min');
+function drop(e) {
+    if (e.parentElement.classList.contains('dropup')) {
+        e.parentElement.classList.remove('dropup');
+        e.parentElement.classList.add('dropdown'); }
+    else if (e.parentElement.classList.contains('dropdown')) {
+        e.parentElement.classList.remove('dropdown');
+        e.parentElement.classList.add('dropup'); }
 }
